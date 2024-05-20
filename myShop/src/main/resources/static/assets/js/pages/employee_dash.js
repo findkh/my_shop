648708571f52abb97da773541f3c22effa3b3054
findk <findkh@nate.com>
2024-05-20 20:21:41 +0900

    2024.05.20 대시보드 내용 추가

--- myShop/src/main/resources/static/assets/js/pages/employee_dash.js
+++ myShop/src/main/resources/static/assets/js/pages/employee_dash.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	getClock(); //맨처음에 한번 실행
 	setInterval(getClock, 1000); //1초 주기로 새로실행
 	
+	
+	getCommuteList()
 });
 
 
@@ -23,22 +25,48 @@ function getDashBoardInfoByEmployee() {
 		url: '/getDashBoardInfoByEmployee',
 		type: 'GET',
 		success: function(response) {
-			
-			console.log(response)
-			
 			if(response.userInfo.userName != undefined){
 				$('#userName').html(response.userInfo.userName);
 			}
 			
-			if(response.code.employee_code != undefined){
-				$('#qrCodeDiv').empty();
-				let qrCodeDiv = $('#qrCodeDiv')[0];
-				let qrCodeText = response.code.employee_code;
-				let qrCode = new QRCode(qrCodeDiv, {
-					text: qrCodeText,
-					width: 300,
-					height: 300
-				});
+			if(response.code != null) {
+				if(response.code.employee_code != undefined){
+					$('#qrCodeDiv').empty();
+					let qrCodeDiv = $('#qrCodeDiv')[0];
+					let qrCodeText = response.code.employee_code;
+					let qrCode = new QRCode(qrCodeDiv, {
+						text: qrCodeText,
+						width: 300,
+						height: 300
+					});
+				}
+			}
+			
+		},
+		error: function(xhr, status, error) {
+			console.error(xhr.responseText);
+		}
+	});
+}
+
+function getCommuteList(){
+	$.ajax({
+		url: '/getCommuteList',
+		type: 'GET',
+		success: function(response) {
+			console.log(response)
+			if(response.length > 0){
+				let tbody = $('#commuteTbody');
+                        tbody.empty(); // Clear any existing rows
+                        $.each(response, function(index, item) {
+                            var row = '<tr>' +
+                                '<td>' + (index + 1) + '</td>' +
+                                '<td>' + item.commute_type + '</td>' +
+                                '<td>' + item.user_name + '</td>' +
+                                '<td>' + item.checked_time + '</td>' +
+                                '</tr>';
+                            tbody.append(row);
+                        });
 			}
 		},
 		error: function(xhr, status, error) {
